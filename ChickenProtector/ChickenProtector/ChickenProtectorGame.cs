ce50fa7b347531cac04b47ec36f29dd7ed284f29
joshkay <josh.m.kay@gmail.com>
2014-11-19 09:52:15 -0500

    Nov. 19 - XNA Project base

--- ChickenProtector/ChickenProtector/ChickenProtectorGame.cs
+++ ChickenProtector/ChickenProtector/ChickenProtectorGame.cs
@@ -46,14 +46,47 @@ namespace ChickenProtector
         /// <summary>Initializes a new instance of the <see cref="StarWarriorGame" /> class.</summary>
         public ChickenProtectorGame()
         {
-            
+            this.elapsedTime = TimeSpan.Zero;
+
+            this.graphics = new GraphicsDeviceManager(this)
+            {
+                IsFullScreen = false,
+                PreferredBackBufferHeight = 720,
+                PreferredBackBufferWidth = 1280,
+                PreferredBackBufferFormat = SurfaceFormat.Color,
+                PreferMultiSampling = false,
+                PreferredDepthStencilFormat = DepthFormat.None
+            };
+#if DEBUG
+            this.graphics.SynchronizeWithVerticalRetrace = false;
+#else
+            this.graphics.SynchronizeWithVerticalRetrace = true;
+#endif
+            this.IsFixedTimeStep = false; //this wont work when vsync is ON
+            this.Content.RootDirectory = "Content";
         }
 
         /// <summary>This is called when the game should draw itself.</summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-           
+            string fps = string.Format("fps: {0}", this.frameRate);
+#if DEBUG
+            string entityCount = string.Format("Active entities: {0}", this.entityWorld.EntityManager.ActiveEntities.Count);
+            string removedEntityCount = string.Format("Removed entities: {0}", this.entityWorld.EntityManager.TotalRemoved);
+            string totalEntityCount = string.Format("Total entities: {0}", this.entityWorld.EntityManager.TotalCreated);
+#endif
+
+            this.GraphicsDevice.Clear(Color.Black);
+            this.spriteBatch.Begin();
+            this.entityWorld.Draw();
+            this.spriteBatch.DrawString(this.font, fps, new Vector2(32, 32), Color.Yellow);
+#if DEBUG
+            this.spriteBatch.DrawString(this.font, entityCount, new Vector2(32, 62), Color.Yellow);
+            this.spriteBatch.DrawString(this.font, removedEntityCount, new Vector2(32, 92), Color.Yellow);
+            this.spriteBatch.DrawString(this.font, totalEntityCount, new Vector2(32, 122), Color.Yellow);
+#endif
+            this.spriteBatch.End();
         }
 
         /// <summary>Allows the game to perform any initialization it needs to before starting to run.
@@ -63,8 +96,22 @@ namespace ChickenProtector
         protected override void Initialize()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
-            //this.spriteBatch = new SpriteBatch(this.GraphicsDevice);
-            //this.font = this.Content.Load<SpriteFont>("myFont");
+            this.spriteBatch = new SpriteBatch(this.GraphicsDevice);
+            this.font = this.Content.Load<SpriteFont>("myFont");
+
+            this.entityWorld = new EntityWorld();
+
+            EntitySystem.BlackBoard.SetEntry("ContentManager", this.Content);
+            EntitySystem.BlackBoard.SetEntry("GraphicsDevice", this.GraphicsDevice);
+            EntitySystem.BlackBoard.SetEntry("SpriteBatch", this.spriteBatch);
+            EntitySystem.BlackBoard.SetEntry("SpriteFont", this.font);
+            EntitySystem.BlackBoard.SetEntry("EnemyInterval", 500);
+
+#if XBOX
+            this.entityWorld.InitializeAll( System.Reflection.Assembly.GetExecutingAssembly());
+#else
+            this.entityWorld.InitializeAll(true);
+#endif
 
             base.Initialize();
         }
@@ -74,6 +121,22 @@ namespace ChickenProtector
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape) ||
+                GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.Back))
+            {
+                this.Exit();
+            }
+
+            this.entityWorld.Update();
+
+            ++this.frameCounter;
+            this.elapsedTime += gameTime.ElapsedGameTime;
+            if (this.elapsedTime > OneSecond)
+            {
+                this.elapsedTime -= OneSecond;
+                this.frameRate = this.frameCounter;
+                this.frameCounter = 0;
+            }
         }      
     }
 }
