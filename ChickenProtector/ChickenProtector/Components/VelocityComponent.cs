929eef3ff47e64de235e58158a39a8099eaf29ce
elite119 <rj.loyel_119@hotmail.com>
2014-11-30 19:47:16 -0500

    Health bar render, and velocity component

---
+++ ChickenProtector/ChickenProtector/Components/VelocityComponent.cs
@@ -0,0 +1,44 @@
+﻿namespace ChickenProtector.Components
+{
+using System;
+using Artemis.Interface;
+
+
+    public class VelocityComponent : IComponent
+    {
+        private const float ToRadians = (float)(Math.PI / 180.0);
+
+        public VelocityComponent()
+            : this(0.0f, 0.0f)
+        {
+        }
+
+        public VelocityComponent(float velocity)
+            : this(velocity, 0.0f)
+        {
+        }
+
+        public VelocityComponent(float velocity, float angle)
+        {
+            this.Speed = velocity;
+            this.Angle = angle;
+        }
+
+        public float Angle { get; set; }
+
+        public float AngleAsRadians
+        {
+            get
+            {
+                return this.AngleAsRadians * ToRadians;
+            }
+        }
+
+        public float Speed { get; set; }
+
+        public void AddAngle(float angle)
+        {
+            this.Angle = (this.AngleAsRadians + angle) % 360;
+        }
+    }
+}
---
+++ ChickenProtector/ChickenProtector/Systems/HealthRenderSystem.cs
@@ -0,0 +1,45 @@
+﻿namespace ChickenProtector.Systems
+{
+    #region Using statements
+
+    using Artemis;
+    using Artemis.Attributes;
+    using Artemis.Manager;
+    using Artemis.System;
+
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    using ChickenProtector.Components;
+
+    #endregion
+
+    [ArtemisEntitySystem(GameLoopType = GameLoopType.Draw, Layer = 0)]
+    public class HealthRenderSystem : EntityProcessingSystem<HealthComponent, TransformComponent>
+    {
+        private SpriteFont font;
+
+        private SpriteBatch spriteBatch;
+
+        public override void LoadContent()
+        {
+            this.spriteBatch = BlackBoard.GetEntry<SpriteBatch>("SpriteBatch");
+            this.font = BlackBoard.GetEntry<SpriteFont>("SpriteFont");
+        }
+
+        public override void Process(Entity entity, HealthComponent healthComponent, TransformComponent transformComponent)
+        {
+            if (healthComponent != null)
+            {
+                if (transformComponent != null)
+                {
+                    string text = healthComponent.HealthPercentage + "%";
+                    float c = (float)(healthComponent.HealthPercentage / 100);
+                    Color color = new Color(1.0f - c, c, 0.0f);
+                    this.spriteBatch.DrawString(this.font, text, new Vector2(transformComponent.X, transformComponent.Y + 25), color, 0.0f, this.font.MeasureString(text) * 0.5f, 1.0f, SpriteEffects.None, 0.5f);
+                }
+            }
+        
+        }
+    }
+}
