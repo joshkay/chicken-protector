3771ecfc3392f51e0352d24fe13873b83cf60445
elite119 <rj.loyel_119@hotmail.com>
2014-12-03 09:21:28 -0500

    MovementSystem

---
+++ ChickenProtector/ChickenProtector/Systems/MovementSystem.cs
@@ -0,0 +1,33 @@
+﻿namespace ChickenProtector.Systems
+{
+#region Using statements
+
+using System;
+
+using Artemis;
+using Artemis.Attributes;
+using Artemis.Manager;
+using Artemis.System;
+
+using ChickenProtector.Components;
+
+#endregion
+
+    [ArtemisEntitySystem(GameLoopType = GameLoopType.Update, Layer = 1)]
+    public class MovementSystem : EntityProcessingSystem<TransformComponent, VelocityComponent>
+    {
+        public override void Process(Entity entity, TransformComponent transformComponent, VelocityComponent velocityComponent)
+        {
+            if (velocityComponent != null)
+            {
+                if (transformComponent != null)
+                {
+                    float ms = TimeSpan.FromTicks(this.EntityWorld.Delta).Milliseconds;
+
+                    transformComponent.X += (float)(Math.Cos(velocityComponent.AngleAsRadians) * velocityComponent.Speed * ms);
+                    transformComponent.Y += (float)(Math.Sin(velocityComponent.AngleAsRadians) * velocityComponent.Speed * ms);
+                }
+            }
+        }
+    }
+}
