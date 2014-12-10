4c6dddceed18b3b29ef5a3620ff6d9a0892ec3d6
elite119 <rj.loyel_119@hotmail.com>
2014-12-09 19:19:07 -0500

    BulletExplosionTemplate, EnemyTemplate

---
+++ ChickenProtector/ChickenProtector/Templates/BulletExplosionTemplate.cs
@@ -0,0 +1,29 @@
+﻿namespace ChickenProtector.Templates
+{
+    #region Using statements
+
+    using Artemis;
+    using Artemis.Attributes;
+    using Artemis.Interface;
+
+    using ChickenProtector.Components;
+
+    #endregion
+
+    [ArtemisEntityTemplate(Name)]
+    public class BulletExplosionTemplate : IEntityTemplate
+    {
+        public const string Name = "BulletExplosionTemplate";
+
+        public Entity BuildEntity(Entity entity, EntityWorld entityWorld, params object[] args)
+        {
+            entity.Group = "EFFECTS";
+
+            entity.AddComponentFromPool<TransformComponent>();
+            entity.AddComponent(new SpatialFormComponent("BulletExplosion"));
+            entity.AddComponent(new ExpiresComponent(1000));
+
+            return entity;
+        }
+    }
+}
---
+++ ChickenProtector/ChickenProtector/Templates/EnemyTemplate.cs
@@ -0,0 +1,32 @@
+﻿namespace ChickenProtector.Templates
+{
+    #region Using statements
+
+    using Artemis;
+    using Artemis.Attributes;
+    using Artemis.Interface;
+
+    using ChickenProtector.Components;
+
+    #endregion
+
+    [ArtemisEntityTemplate(Name)]
+    public class EnemyTemplate : IEntityTemplate
+    {
+        public const string Name = "EnemyShipTemplate";
+
+        public Entity BuildEntity(Entity entity, EntityWorld entityWorld, params object[] args)
+        {
+            entity.Group "";
+
+            entity.AddComponentFromPool<TransformComponent>();
+            entity.AddComponent(new SpatialFormComponent(" "));
+            entity.AddComponent(new HealthComponent(10));
+            entity.AddComponent(new WeaponComponent());
+            entity.AddComponent(new EnemyComponent());
+            entity.AddComponent(new VelocityComponent());
+            
+            return entity;
+        }
+    }
+}
